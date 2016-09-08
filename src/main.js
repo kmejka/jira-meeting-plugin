d35e27e83ab1d6865bf2c658f6957540881d66f8
Michał Jezierski <ambinanitelo@gmail.com>
2016-09-08 13:17:47 +0200

    date inputs

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log('sdfsdfsdf');
+import Flatpickr from 'flatpickr';
+
+const dateFromPicker = document.querySelector('.date-from');
+const flatpicker = new Flatpickr(dateFromPicker);
+
+const submitButton = document.querySelector('.create-meeting-button');
+submitButton.addEventListener('click', () => {
+   console.log(flatpicker.formatDate());
+});
